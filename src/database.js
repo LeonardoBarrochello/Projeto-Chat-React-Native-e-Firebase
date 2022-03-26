6b60b7075f0b25f8f17655e04cde3983e651243b
Leonardo <leonardobarrochello58@gmail.com>
2022-03-25 23:59:02 -0300

    Projeto chat concluido

--- src/database.js
+++ src/database.js
@@ -1,13 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAamWN0xXD5ASnjlLv-mJZGCMIoEj9QeG4",
-  authDomain: "simple-chat-78f87.firebaseapp.com",
-  databaseURL: "https://simple-chat-78f87.firebaseio.com",
-  projectId: "simple-chat-78f87",
-  storageBucket: "simple-chat-78f87.appspot.com",
-  messagingSenderId: "976947114024",
-  appId: "1:976947114024:web:c6a57f2d2864e147fdc51d",
+  apiKey: "AIzaSyB9GRL-aHa8I-OACuEfs1sc1EV4qPT5zSA",
+  authDomain: "mychat-app-50133.firebaseapp.com",
+  databaseURL : "https://mychat-app-50133-default-rtdb.firebaseio.com",
+  projectId: "mychat-app-50133",
+  storageBucket: "mychat-app-50133.appspot.com",
+  messagingSenderId: "116402439530",
+  appId: "1:116402439530:web:b0b719fb7c9695a66ad113",
+  measurementId: "G-XH1Xa3T1W32"
 };
 
 firebase.initializeApp(firebaseConfig);
